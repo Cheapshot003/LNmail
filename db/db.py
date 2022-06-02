f89fbb61c6bd79edcbebfdfd35c58a108fc0c26e
DerCheapi <oletietz@protonmail.com>
2022-06-02 14:49:26 +0200

    Debugging

--- db/db.py
+++ db/db.py
@@ -4,6 +4,6 @@ def get_hash(chatid):
     con = sqlite3.connect("../db/payments.db")
     cur = con.cursor()
     daten = cur.execute("SELECT rhash FROM payments WHERE chat_id=\'" + str(chatid) + "\'")
-    print(daten[0][0])
+    print(daten(0)[0])
 
 get_hash(34534524)
