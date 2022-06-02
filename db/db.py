cb335c37cce1ba1af44b450d11de3358a1dac654
DerCheapi <oletietz@protonmail.com>
2022-06-02 14:52:22 +0200

    Debugging

--- db/db.py
+++ db/db.py
@@ -4,6 +4,13 @@ def get_hash(chatid):
     con = sqlite3.connect("../db/payments.db")
     cur = con.cursor()
     daten = cur.execute("SELECT rhash FROM payments WHERE chat_id=\'" + str(chatid) + "\'")
-    print(daten(0)[0])
+    con.close()
+    for i in daten:
+        return i[0]
 
-get_hash(34534524)
+def save_hash(chatid, rhash):
+    con = sqlite3.connect("../db/payments.db")
+    cur = con.cursor()
+    cur.execute("INSERT INTO payments VALUES (\'" + str(chatid) + "\',\'" + str(rhash) + "\')")
+    con.commit()
+    con.close()
